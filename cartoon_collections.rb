463718ff4e4454998a377e470d6efe820f68feda
Randy Steele <steelerx155@gmail.com>
2019-10-19 18:54:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)   
-  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarves = {"Doc", "Dopey", "Bashful", "Grumpy"}
   dwarves.each_with_index do  |name, index| 
   dwarves[name] = index
    puts "#{index} #{dwarves}"
