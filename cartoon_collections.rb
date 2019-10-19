b83400297a2a73a2e3813837057df2b4e1915a9a
Randy Steele <steelerx155@gmail.com>
2019-10-19 18:45:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-def roll_call_dwarves   
+def roll_call_dwarves(dwarves)   
   dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
   dwarves.each_with_index(1) do  |name, index| 
   dwarves[name] = index
