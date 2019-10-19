fd5697da46da166608e2e8bf4d48b27d24e40439
Randy Steele <steelerx155@gmail.com>
2019-10-19 19:43:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(veggies)# code an argument here
 
 end
 
-def long_planteer_calls(long_planteer_calls)# code an argument here
+def long_planeteer_calls(long_planteer_calls)# code an argument here
   # Your code here
   answer = false
   long_planteer_calls.each do |call|
